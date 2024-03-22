c9b429dfe9a16685b2038fcfcea438b597ab37b3
nikita <samaraphone@yandex.ru>
2024-03-22 11:43:59 +0300

    add homework-vue-3

--- homeworks/layout/scripts/script.js
+++ homeworks/layout/scripts/script.js
@@ -1,48 +1,48 @@
 new Vue({
     el: "#app",
     data: {
-        articles: [
+        listNews: [
             {
                 id: 1,
                 tag: "Kitchan Design",
                 image: "./img/kitchen_four_chez.png",
                 title: "Let’s Get Solution For Building Construction Work",
-                dateNews: "26 December,2022",
+                date: "26 December,2022",
             },
             {
                 id: 2,
                 tag: "Living Design",
                 image: "./img/kitchen_with_view_door.png",
                 title: "Low Cost Latest Invented Interior Designing Ideas.",
-                dateNews: "22 December,2022",
+                date: "22 December,2022",
             },
             {
                 id: 3,
                 tag: "Interior Design",
                 image: "./img/kitchen_sink_hood.png",
                 title: "Best For Any Office & Business Interior Solution",
-                dateNews: "25 December,2022",
+                date: "25 December,2022",
             },
             {
                 id: 4,
                 tag: "Kitchan Design",
                 image: "./img/kitchen_beige_four_lamp.png",
                 title: "Let’s Get Solution For Building Construction Work",
-                dateNews: "26 December,2022",
+                date: "26 December,2022",
             },
             {
                 id: 5,
                 tag: "Living Design",
                 image: "./img/room_with_two_chair_one_sofa.png",
                 title: "Low Cost Latest Invented Interior Designing Ideas.",
-                dateNews: "22 December,2022",
+                date: "22 December,2022",
             },
             {
                 id: 6,
                 tag: "Interior Design",
                 image: "./img/kitchen_four_chez.png",
                 title: "Best For Any Office & Business Interior Solution",
-                dateNews: "25 December,2022",
+                date: "25 December,2022",
             },
             {
                 id: 7,
@@ -50,9 +50,21 @@ new Vue({
                 image: "./img/kitche_with_marble_island.png",
                 title: "Low Cost Latest Invented Interior Designing Ideas",
                 text: "Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpis dignissim maximus.posuere in.Contrary to popular belief. Lorem Ipsum is not simply random text. It has roots in a piece of classica.",
-                dateNews: "26 December,2022",
+                date: "26 December,2022",
             },
         ],
     },
 
 });
+
+// Vue.component('articles-component', {
+//     template: `<p>Hi</p>`,
+
+//     data() {
+//         return {
+
+//         }
+//     },
+
+
+// });
